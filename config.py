c1c028ff251abe63a55739f576b0b7c97cb3e733
zhujiawei <aishenghuomeidaoli@163.com>
2016-01-20 20:46:12 +0800

    20160120

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ class Config:
 	SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN=True
 	FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
-	FLASKY_MAIL_SENDER='aishenghuomeidaoli@163.com' or \
-	'Flasky Admin'+'<'+os.environ.get('FLASKY_MAIL_SENDER')+'>' 
-	FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN') or 'aishenghuomeidaoli@163.com'
+	FLASKY_MAIL_SENDER='Flasky Admin'+'<'+os.environ.get('FLASKY_MAIL_SENDER')+'>' 
+	FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
 	FLASKY_POSTS_PER_PAGE = 20
 	FLASKY_FOLLOWERS_PER_PAGE = 50
 	FLASK_COMMENTS_PER_AGE=10
@@ -21,8 +20,8 @@ class DevelopmentConfig(Config):
 	MAIL_SERVER='smtp.163.com' or os.environ.get('MAIL_SERVER')
 	MAIL_PORT=25 or os.environ.get('MAIL_PORT')
 	MAIL_USE_LTS=True
-	MAIL_USERNAME=os.environ.get('MAIL_USERNAME') or 'aishenghuomeidaoli@163.com'
-	MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD') or 'zhujiawei'
+	MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
 	SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL') or \
 	'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
